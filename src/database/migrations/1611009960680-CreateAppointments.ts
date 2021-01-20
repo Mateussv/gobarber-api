b8c3c2bd1ee02bfd546a4eb0207c3b2ceb04b240
Mateus Victor <mateus.svictor@gmail.com>
2021-01-19 21:10:27 -0300

    fix: set uuid as default id generator

--- src/database/migrations/1611009960680-CreateAppointments.ts
+++ src/database/migrations/1611009960680-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1611009960680
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
